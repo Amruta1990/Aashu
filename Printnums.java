9332ef4dea40d57756b75266871c4bc81c50d157
Amruta1990 <118837878+Amruta1990@users.noreply.github.com>
2022-12-20 12:58:24 +0530

    Add files via upload
    task done

---
+++ Printnums.java
@@ -0,0 +1,17 @@
+package Print1ToNnumber;
+
+import java.util.Scanner;
+
+public class Printnums {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter value of n");
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			System.out.println(i);
+			
+		}
+		
+	}
+
+}
